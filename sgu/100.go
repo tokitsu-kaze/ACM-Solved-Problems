f68bbfbdf700f487f6f03e25542a3212712a5674
tokitsukaze <861794979@qq.com>
2018-06-25 21:20:15 +0800

    Add files via upload

---
+++ sgu/100.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+import "strconv"
+
+func solve(){
+    var a,b int
+    a=readInt()
+    b=readInt()
+    fmt.Println(a+b)
+}
+
+
+var scanner *bufio.Scanner
+
+func main() {
+	scanner = bufio.NewScanner(os.Stdin)
+	const MaxTokenLength = 1024 * 1024
+	scanner.Buffer(make([]byte, 0, MaxTokenLength), MaxTokenLength)
+	scanner.Split(bufio.ScanWords)
+	solve()
+}
+func readString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func readInt() int {
+	val, _ := strconv.Atoi(readString())
+	return val
+}
+
+func readInt64() int64 {
+	v, _ := strconv.ParseInt(readString(), 10, 64)
+	return v
+}
