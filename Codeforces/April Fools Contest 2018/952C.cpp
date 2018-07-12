64cf32e353c4a07ded7bc9f0d5e0464c6311fac3
tokitsu-kaze <861794979@qq.com>
2018-07-12 20:51:48 +0800

    update

---
+++ Codeforces/April Fools Contest 2018/952C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+using namespace std;
+#define _GLIBCXX_PERMIT_BACKWARD_HASH
+#include <ext/hash_map>
+using namespace __gnu_cxx;
+struct str_hash{size_t operator()(const string& str)const{return __stl_hash_string(str.c_str());}};
+typedef long long ll;
+typedef unsigned long long ull;
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define PDD pair<double,double>
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-8;
+const int MAX=1e5+10;
+const ll mod=1e9+7;
+int main()
+{
+	int n,i,x,flag=0,pre;
+	cin>>n;
+	cin>>pre;
+	n--;
+	while(n--)
+	{
+		cin>>x;
+		flag+=(abs(x-pre)>=2);
+		pre=x;
+	}
+	flag?puts("NO"):puts("YES");
+	return 0;
+} 
---
+++ Codeforces/April Fools Contest 2018/952D.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+using namespace std;
+#define _GLIBCXX_PERMIT_BACKWARD_HASH
+#include <ext/hash_map>
+using namespace __gnu_cxx;
+struct str_hash{size_t operator()(const string& str)const{return __stl_hash_string(str.c_str());}};
+typedef long long ll;
+typedef unsigned long long ull;
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define PDD pair<double,double>
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-8;
+const int MAX=1e5+10;
+const ll mod=1e9+7;
+int main()
+{
+	srand(time(0));
+	int x=rand()%2;
+	if(x) puts("Odd");
+	else puts("Even");
+	return 0;
+} 
