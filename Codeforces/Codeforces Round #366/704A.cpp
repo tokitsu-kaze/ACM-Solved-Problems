dd6f9503318a8bd218ca8f0ca2210f76ea55cb3b
tokitsu-kaze <861794979@qq.com>
2018-07-20 16:45:49 +0800

    update

---
+++ Codeforces/Codeforces Round #366/704A.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) x.size()
+#define all(x) x.begin(),x.end()
+using namespace std;
+#define _GLIBCXX_PERMIT_BACKWARD_HASH
+#include <ext/hash_map>
+using namespace __gnu_cxx;
+struct str_hash{size_t operator()(const string& str)const{return __stl_hash_string(str.c_str());}};
+typedef long long ll;
+typedef unsigned long long ull;
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define PDD pair<double,double>
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=3e5+10;
+const ll mod=1e9+7;
+int flag[MAX],cnt[MAX];
+int main()
+{
+	int n,qq,i,op,x,now,ans,sz; 
+	while(~scanf("%d%d",&n,&qq))
+	{
+		mem(flag,0);
+		mem(cnt,0);
+		queue<int> q;
+		now=ans=0;
+		while(qq--)
+		{
+			sz=sz(q);
+			scanf("%d%d",&op,&x);
+			if(op==1)
+			{
+				q.push(x);
+				ans++;
+				cnt[x]++;
+			}
+			else if(op==2)
+			{
+				ans-=cnt[x];
+				cnt[x]=0;
+				flag[x]=now+sz;
+			}
+			else
+			{
+				int t=x-now;
+				while(t>0)
+				{
+					t--;
+					x=q.front();
+					q.pop();
+					now++;
+					if(now>flag[x])
+					{
+						cnt[x]--;
+						ans--;
+						flag[x]=now;
+					}
+				}
+			}
+			printf("%d\n",ans);
+		}
+	}
+	return 0;
+}
