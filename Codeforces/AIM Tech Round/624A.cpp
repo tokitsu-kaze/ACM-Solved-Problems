6e0c9dffee9ff3a8e9e795c4c47d3eff157c44a0
tokitsu-kaze <861794979@qq.com>
2018-07-20 17:30:54 +0800

    update

---
+++ Codeforces/AIM Tech Round/624A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <cctype>
+#include <map>
+#include <list>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+const int INF=0x3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=1e6+10;
+const ll mod=1e6;
+int main()
+{
+	double d,r,v1,v2,t;
+	while(~scanf("%lf%lf%lf%lf",&d,&r,&v1,&v2))
+	{
+		printf("%.10lf\n",(r-d)/(v1+v2));
+	}
+	return 0;
+}
---
+++ Codeforces/AIM Tech Round/624B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <cctype>
+#include <map>
+#include <list>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+const int INF=0x3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=1e6+10;
+const ll mod=1e6;
+int main()
+{
+	ll n,a[33],i,ans;
+	while(~scanf("%lld",&n))
+	{
+		map<ll,ll> mp;
+		for(i=0;i<n;i++)
+		{
+			scanf("%lld",&a[i]);
+		}
+		sort(a,a+n);
+		ans=0;
+		for(i=0;i<n;i++)
+		{
+			while(mp[a[i]]&&a[i]>0) a[i]--;
+			mp[a[i]]=1;
+			ans+=a[i];
+		}
+		printf("%lld\n",ans);
+	}
+	return 0;
+}
