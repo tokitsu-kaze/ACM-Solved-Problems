e82ac1765c47e927e34b5095b2a80063e0e58a30
tokitsu-kaze <861794979@qq.com>
2018-07-27 19:30:44 +0800

    update

---
+++ hdu/6312.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-9;
+const int MAX=1e5+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+int main()
+{
+	int n;
+	while(~scanf("%d",&n))
+	{
+		puts("Yes");
+	}
+	return 0;
+}
