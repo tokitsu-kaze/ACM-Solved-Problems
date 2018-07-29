93a3707d61a56068aa03e91aa102fed9f869d569
tokitsu-kaze <861794979@qq.com>
2018-07-30 02:17:01 +0800

    update

---
+++ Other Contest/牛客练习赛9/D - 珂朵莉的假toptree.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) x.size()
+#define all(x) x.begin(),x.end()
+using namespace std;
+#define _GLIBCXX_PERMIT_BACKWARD_HASH
+#include <ext/hash_map>
+using namespace __gnu_cxx;
+struct str_hash{size_t operator()(const string& str)const{return __stl_hash_string(str.c_str());}};
+typedef long long ll;
+typedef unsigned long long ull;
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+#define PDD pair<double,double>
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-8;
+const int MAX=2e5+10;
+const ll mod=1e9+7;
+int main()
+{
+	int n,i;
+	char temp[111];
+	string res;
+	for(i=1;;i++)
+	{
+		if(res.length()>1000) break;
+		sprintf(temp,"%d",i);
+		res+=temp;
+	}
+	while(~scanf("%d",&n))
+	{
+		printf("%c\n",res[n-1]);
+	}
+	return 0;
+}
