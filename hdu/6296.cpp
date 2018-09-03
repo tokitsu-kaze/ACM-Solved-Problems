b7a93c4fd495972cf756c530ce83f17c0a738580
tokitsu-kaze <861794979@qq.com>
2018-09-03 20:58:12 +0800

    update

---
+++ hdu/6296.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+//	inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;} 
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true; 
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze 
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=1e5+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+int x[MAX][2],y[MAX][2];
+ll bit[1005][1005],c3[MAX];
+void go()
+{
+	int t,n,i,j;
+	for(i=1;i<=MAX-10;i++) c3[i]=1LL*i*(i-1)*(i-2)/6;
+	read(t);
+	while(t--)
+	{
+		read(n);
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<2;j++)
+			{
+				read(x[i][j],y[i][j]);
+			}
+		}
+		auto gao=[&](int dx,int dy)->ll
+		{
+			mem(bit,0);
+			int x1,y1,x2,y2;
+			ll res=0;
+			for(i=0;i<n;i++)
+			{
+				x1=x[i][0]+dx;
+				y1=y[i][0]+dy;
+				x2=x[i][1];
+				y2=y[i][1];
+				if(x1>x2||y1>y2) continue;
+				bit[x1][y1]++;
+				bit[x1][y2+1]--;
+				bit[x2+1][y1]--;
+				bit[x2+1][y2+1]++;
+			}
+			for(i=1;i<=1000;i++)
+			{
+				for(j=1;j<=1000;j++)
+				{
+					bit[i][j]+=bit[i-1][j]+bit[i][j-1]-bit[i-1][j-1];
+					res+=c3[bit[i][j]];
+				}
+			}
+			return res;
+		};
+		printf("%lld\n",gao(0,0)-gao(1,0)-gao(0,1)+gao(1,1));
+	}
+}
+
