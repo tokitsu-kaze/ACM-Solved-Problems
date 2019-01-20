572a8a16e62106839e6af8d8128cb2b69cd6143a
tokitsu-kaze <861794979@qq.com>
2019-01-20 13:51:12 +0800

    Create 3374.cpp

---
+++ hdu/3374.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+//	inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;} 
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true; 
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze 
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=1e6+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+int nex[MAX];
+void get_next(char *s,int *nex,int len)
+{
+	int i,j;
+	i=0;
+	j=nex[0]=-1;
+	while(i<len)
+	{
+		if(j==-1||s[i]==s[j]) nex[++i]=++j;
+		else j=nex[j];
+	}
+}
+int min_representation(char *s)
+{
+	int i=0,j=1,k=0;
+	int len=strlen(s);
+	while(i<len&&j<len&&k<len)
+	{
+		if(s[(i+k)%len]==s[(j+k)%len])k++;
+		else
+		{
+			if(s[(i+k)%len]>s[(j+k)%len])i=i+k+1;
+			else j=j+k+1;
+			if(i==j)j++;
+			k=0;
+		}
+	}
+	return i<j?i:j;
+}
+int max_representation(char *s)
+{
+	int i,j,k,len,t;
+	len=strlen(s);
+	i=k=0;
+	j=1;
+	while(i<len&&j<len&&k<len)
+	{
+		t=s[(i+k)%len]-s[(j+k)%len];
+		if(!t) k++;
+		else
+		{
+			if(t>0)
+			{
+				if(j+k+1>i) j=j+k+1;
+				else j=i+1;
+			}
+			else if(i+k+1>j) i=i+k+1;
+			else i=j+1;
+			k=0;
+		}
+	}
+	return i<j?i:j;
+}
+char s[MAX];
+void go()
+{
+	int len,loop;
+	while(read(s))
+	{
+		len=strlen(s);
+		get_next(s,nex,len);
+		if(len%(len-nex[len])==0) loop=len/(len-nex[len]);
+		else loop=1;
+		printf("%d %d %d %d\n",min_representation(s)+1,loop,max_representation(s)+1,loop);
+	}
+} 
