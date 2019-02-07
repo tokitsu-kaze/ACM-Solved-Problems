a6c27591e8d69a54cca85659d22f3cf5ba6d566e
tokitsu-kaze <861794979@qq.com>
2019-02-08 02:56:17 +0800

    Create 牛可乐发红包脱单ACM赛$

---
+++ nowcoder/牛可乐发红包脱单ACM赛$/223A - 生成树.cpp
@@ -0,0 +1,46 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3f;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=2e6+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+int main()
+{
+	int n,i,a,b,ans;
+	while(~scanf("%d",&n))
+	{
+		map<PII,int> mp;
+		for(i=1;i<n;i++)
+		{
+			scanf("%d%d",&a,&b);
+			if(a<b) swap(a,b);
+			mp[MP(a,b)]=1;
+		}
+		ans=0;
+		for(i=1;i<n;i++)
+		{
+			scanf("%d%d",&a,&b);
+			if(a<b) swap(a,b);
+			ans+=mp[MP(a,b)];
+		}
+		printf("%d\n",n-ans-1);
+	}
+	return 0;
+}
---
+++ nowcoder/牛可乐发红包脱单ACM赛$/223C - 区区区间间间.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e6 + 5;
+typedef long long int ll;
+
+int n;
+int v[N], l[N], r[N];
+
+ll solve (bool flag) {
+	if (flag)
+		for (int i = 0; i < n; ++i)
+			v[i] = -v[i];
+	for (int i = 0; i < n; ++i)
+		l[i] = r[i] = i;
+	for (int i = 1; i < n; ++i) {
+		int j = i;
+		while (j and v[i] >= v[j - 1])
+			j = l[j - 1];
+		l[i] = j;
+	}
+	for (int i = n - 2; i >= 0; --i) {
+		int j = i;
+		while (j < n - 1 and v[i] > v[j + 1])
+			j = r[j + 1];
+		r[i] = j;
+	}
+	ll ans = 0;
+	for (int i = 0; i < n; ++i)
+		ans += ll(v[i]) * (i - l[i] + 1) * (r[i] - i + 1);
+	return ans;
+}
+
+int main () {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d", &n);
+		for (int i = 0; i < n; ++i)
+			scanf("%d", &v[i]);
+		printf("%lld\n", solve(0) + solve(1));
+	}
+	return 0;
+}
