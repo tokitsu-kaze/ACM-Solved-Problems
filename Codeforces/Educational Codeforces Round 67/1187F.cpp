4bd8cbd02f03849762725f98ddf269498db93655
tokitsu-kaze <861794979@qq.com>
2019-07-02 21:49:52 +0800

    Create 1187F.cpp

---
+++ Codeforces/Educational Codeforces Round 67/1187F.cpp
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+//  inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;}
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true;
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=2e5+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+ll l[MAX],r[MAX];
+ll pow2(ll a,ll b)
+{
+	ll res=1;
+	while(b)
+	{
+		if(b&1) res=res*a%mod;
+		a=a*a%mod;
+		b>>=1;
+	}
+	return res;
+}
+ll inv(ll x)
+{
+	return pow2(x,mod-2);
+}
+ll a[MAX],b[MAX];
+void go()
+{
+	int n,i;
+	ll ans,sum,tmp;
+	while(read(n))
+	{
+		read(l,1,n);
+		read(r,1,n);
+		mem(a,0);
+		a[1]=0;
+		for(i=2;i<=n;i++)
+		{
+			a[i]=max(0LL,min(r[i],r[i-1])-max(l[i],l[i-1])+1)
+				*inv((r[i-1]-l[i-1]+1)*(r[i]-l[i]+1)%mod)%mod;
+		}
+		mem(b,0);
+		for(i=1;i<=n;i++)
+		{
+			b[i]=(1-a[i]-a[i+1])%mod;
+			if(i>1&&i<n)
+			{
+				b[i]+=max(0LL,min({r[i-1],r[i],r[i+1]})-max({l[i-1],l[i],l[i+1]})+1)
+					*inv((r[i-1]-l[i-1]+1)*(r[i]-l[i]+1)%mod*(r[i+1]-l[i+1]+1)%mod)%mod;
+			}
+			b[i]%=mod;
+		}
+		for(i=1;i<=n;i++) a[i]=1-a[i];
+		sum=0;
+		for(i=1;i<=n;i++) sum=(sum+a[i])%mod;
+		ans=0;
+		for(i=1;i<=n;i++)
+		{
+			
+			ans=(ans+a[i])%mod;
+			tmp=(sum-a[i])%mod;
+			if(i-1>=1) tmp=(tmp-a[i-1])%mod;
+			if(i+1<=n) tmp=(tmp-a[i+1])%mod;
+			ans=(ans+a[i]*tmp%mod)%mod;
+			if(i+1<=n) ans=(ans+b[i])%mod;
+			if(i-1>=1) ans=(ans+b[i-1])%mod;
+		}
+		if(ans<0) ans+=mod;
+		printf("%lld\n",ans);
+	}
+} 
