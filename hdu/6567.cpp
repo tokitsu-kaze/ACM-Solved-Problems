40de2b308d4dd91306fcda8abf860af10750c207
tokitsu-kaze <861794979@qq.com>
2019-07-23 19:07:30 +0800

    Create 2019CCPC-江西省赛

---
+++ hdu/6567.cpp
@@ -0,0 +1,216 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+	//inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;}
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true;
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=1e5+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+struct dsu
+{
+	int pre[MAX];
+	void init(int n)
+	{
+		int i;
+		for(i=1;i<=n;i++) pre[i]=i;
+	}
+	int find(int x)
+	{
+		if(pre[x]!=x) pre[x]=find(pre[x]);
+		return pre[x];
+	}
+	bool merge(int a,int b)
+	{
+		int ra,rb;
+		ra=find(a);
+		rb=find(b);
+		if(ra!=rb)
+		{
+			pre[ra]=rb;
+			return 1;
+		}
+		return 0;
+	}
+}dsu;
+VI mp[MAX];
+int sz[MAX],mn,ct[2],n,sum;
+void dfs0(int x,int fa)
+{
+	sum++;
+	for(auto to:mp[x])
+	{
+		if(to==fa) continue;
+		dfs0(to,x);
+	}
+}
+void dfs(int x,int fa,int flag)
+{
+	int res=0;
+	sz[x]=1;
+	for(auto to:mp[x])
+	{
+		if(to==fa) continue;
+		dfs(to,x,flag);
+		sz[x]+=sz[to];
+		res=max(res,sz[to]);
+	}
+	res=max(res,sum-sz[x]);
+	if(res<mn)
+	{
+		mn=res;
+		ct[flag]=x;
+	}
+}
+ll ans;
+void gao(int x,int fa)
+{
+	sz[x]=1;
+	for(auto to:mp[x])
+	{
+		if(to==fa) continue;
+		gao(to,x);
+		sz[x]+=sz[to];
+		ans+=1ll*sz[to]*(n-sz[to]);
+	}
+}
+void go()
+{
+	int i,rt[2],a,b;
+	while(read(n))
+	{
+		dsu.init(n);
+		for(i=1;i<=n;i++) mp[i].clear();
+		for(i=1;i<=n-2;i++)
+		{
+			read(a,b);
+			dsu.merge(a,b);
+			mp[a].pb(b);
+			mp[b].pb(a);
+		}
+		set<int> s;
+		for(i=1;i<=n;i++) s.insert(dsu.find(i));
+		assert(sz(s)==2);
+		rt[0]=*s.begin();
+		rt[1]=*s.rbegin();
+		for(i=0;i<2;i++)
+		{
+			sum=0;
+			dfs0(rt[i],0);
+			mn=INF;
+			dfs(rt[i],0,i);
+		}
+		mp[ct[0]].pb(ct[1]);
+		mp[ct[1]].pb(ct[0]);
+	//	debug(ct[0],ct[1])
+		ans=0;
+		gao(1,0);
+		printf("%lld\n",ans);
+	}
+}
