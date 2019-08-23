0ecbf503b22094c86ff5359b9e7603b509444363
tokitsu-kaze <861794979@qq.com>
2019-08-24 02:34:20 +0800

    Create Educational Codeforces Round 71

---
+++ Codeforces/Educational Codeforces Round 71/1207G.cpp
@@ -0,0 +1,272 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+	//inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;}
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true;
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef vector<PII > VPII;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-8;
+const int MAX=4e5+10;
+const ll mod=998244353;
+/*********************************  head  *********************************/
+struct Fenwick_Tree
+{
+	#define type int
+	type bit[MAX];
+	int n;
+	void init(int _n){n=_n;mem(bit,0);}
+	int lowbit(int x){return x&(-x);}
+	void insert(int x,type v)
+	{
+		while(x<=n)
+		{
+			bit[x]+=v;
+			x+=lowbit(x);
+		}
+	}
+	type get(int x)
+	{
+		type res=0;
+		while(x)
+		{
+			res+=bit[x];
+			x-=lowbit(x);
+		}
+		return res;
+	}
+	type ask(int l,int r)
+	{
+		if(l-1<=0) return get(r);
+		return get(r)-get(l-1);
+	}
+	#undef type
+}tr;
+struct AC_Automaton
+{
+	static const int K=26;//may need change
+	int nex[MAX][K],fail[MAX],cnt[MAX],last[MAX];
+	int root,tot;
+	inline int getid(char c){return c-'a';}//may need change
+	int newnode()
+	{
+		mem(nex[tot],0);
+		fail[tot]=0;
+		cnt[tot]=0;
+		return tot++;
+	}
+	void init()
+	{
+		tot=0;
+		root=newnode();
+	}
+	int insert(char *s)
+	{
+		int len,now,i;
+		len=strlen(s);
+		now=root;
+		for(i=0;i<len;i++)
+		{
+			int t=getid(s[i]);
+			if(!nex[now][t]) nex[now][t]=newnode();
+			now=nex[now][t];
+		}
+		cnt[now]++;
+		return now;
+	}
+	void work()
+	{
+		int i,now;
+		queue<int>q;
+		for(i=0;i<K;i++)
+		{
+			if(nex[root][i]) q.push(nex[root][i]);
+		}
+		while(!q.empty())
+		{
+			now=q.front();
+			q.pop();
+			if(cnt[fail[now]]) last[now]=fail[now];
+			else last[now]=last[fail[now]];
+			for(i=0;i<K;i++)
+			{
+				if(nex[now][i])
+				{
+					fail[nex[now][i]]=nex[fail[now]][i];
+					q.push(nex[now][i]);
+				}
+				else nex[now][i]=nex[fail[now]][i];
+			}
+		}
+	}
+	vector<int> mp[MAX];
+	int l[MAX],r[MAX],tt;
+	void dfs(int x)
+	{
+		l[x]=++tt;
+		for(auto to:mp[x]) dfs(to);
+		r[x]=tt;
+	}
+	void build_tree()
+	{
+		for(int i=0;i<=tot;i++) mp[i].clear();
+		for(int i=1;i<tot;i++) mp[fail[i]].pb(i);
+		tt=0;
+		dfs(root);
+	}
+	
+}ac;
+int op[MAX],ans[MAX];
+VI mp[MAX];
+VPII qst[MAX];
+char c[MAX],s[MAX];
+void dfs(int x,int now)
+{
+	now=ac.nex[now][ac.getid(c[x])];
+	tr.insert(ac.l[now],1);
+	for(auto to:mp[x]) dfs(to,now);
+	for(auto it:qst[x]) ans[it.se]=tr.ask(ac.l[it.fi],ac.r[it.fi]);
+	tr.insert(ac.l[now],-1);
+}
+void go()
+{
+	int n,q,i,x;
+	while(read(n))
+	{
+		for(i=1;i<=n;i++)
+		{
+			mp[i].clear();
+			qst[i].clear();
+		}
+		for(i=1;i<=n;i++)
+		{
+			read(op[i]);
+			if(op[i]==1) read(c[i]);
+			else
+			{
+				read(x,c[i]);
+				mp[x].pb(i);
+			}
+		}
+		ac.init();
+		read(q);
+		for(i=1;i<=q;i++)
+		{
+			read(x);
+			read(s);
+			qst[x].pb(MP(ac.insert(s),i));
+		}
+		ac.work();
+		ac.build_tree();
+		tr.init(ac.tot);
+		for(i=1;i<=n;i++)
+		{
+			if(op[i]==1) dfs(i,ac.root);
+		}
+		for(i=1;i<=q;i++) printf("%d\n",ans[i]);
+	}
+}
