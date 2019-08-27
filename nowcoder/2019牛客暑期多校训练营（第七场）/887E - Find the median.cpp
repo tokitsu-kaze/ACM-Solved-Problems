d224ff5fd33ba29f3ed758177171cdf25d2e5bfe
tokitsu-kaze <861794979@qq.com>
2019-08-27 20:28:10 +0800

    Create 2019牛客暑期多校训练营（第七场）

---
+++ nowcoder/2019牛客暑期多校训练营（第七场）/887E - Find the median.cpp
@@ -0,0 +1,249 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+//	inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;} 
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true; 
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze 
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=3e6+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+struct Discretization
+{
+	#define type int
+	vector<type> a;
+	void init(){a.clear();}
+	void add(type x){a.pb(x);}
+	void work(){sort(all(a));a.resize(unique(all(a))-a.begin());}
+	int get_pos(type x){return lower_bound(all(a),x)-a.begin()+1;}
+	type get_val(int pos){return a[pos-1];}
+	int size(){return a.size();}
+	#undef type
+}d;
+struct Segment_Tree
+{
+	#define type ll
+	#define ls (id<<1)
+	#define rs (id<<1|1)
+	int n,ql,qr;
+	type v[MAX<<2],tag[MAX<<2],qv;
+	void pushup(int id)
+	{
+		v[id]=v[ls]+v[rs];
+	}
+	int get(int x){return d.get_val(x);}
+	void pushdown(int l,int r,int id)
+	{
+		if(!tag[id]) return;
+		tag[ls]+=tag[id];
+		tag[rs]+=tag[id];
+		int mid=(l+r)>>1;
+		ll ll,rr,md;
+		ll=get(l);
+		rr=get(r);
+		md=get(mid);
+		v[ls]+=(md-ll)*tag[id];
+		v[rs]+=(rr-md)*tag[id];
+		tag[id]=0;
+	}
+	void build(int l,int r,int id)
+	{
+		tag[id]=0;
+		if(l+1==r) return;
+		int mid=(l+r)>>1;
+		build(l,mid,ls);
+		build(mid,r,rs);
+		pushup(id);
+	}
+	void update(int l,int r,int id)
+	{
+		if(l>=ql&&r<=qr)
+		{
+			v[id]+=get(r)-get(l);
+			tag[id]++;
+			return;
+		}
+		pushdown(l,r,id);
+		int mid=(l+r)>>1;
+		if(ql<mid) update(l,mid,ls);
+		if(qr>mid) update(mid,r,rs);
+		pushup(id);
+	}
+	ll now;
+	int query(int l,int r,int id)
+	{
+		if(l+1==r)
+		{
+			ll tmp=v[id]/(get(r)-get(l));
+	//		debug(now,get(l),tmp)
+			return get(l)+(now+tmp-1)/tmp-1;
+		}
+		pushdown(l,r,id);
+		int mid=(l+r)>>1;
+		if(v[ls]>=now) return query(l,mid,ls);
+		else
+		{
+			now-=v[ls];
+			return query(mid,r,rs);
+		}
+	}
+	void build(int _n){n=_n;build(1,n,1);}
+	void upd(int l,int r,type v)
+	{
+		ql=l;
+		qr=r;
+		qv=v;
+		update(1,n,1);
+	}
+	int ask()
+	{
+		now=(v[1]+1)/2;
+	//	debug(now)
+		return query(1,n,1);
+	}
+	#undef type
+	#undef ls
+	#undef rs
+}tr;
+int x[MAX],y[MAX],l[MAX],r[MAX];
+void go()
+{
+	int n,i;
+	int a[5],b[5],c[5],m[5];
+	while(read(n))
+	{
+		read(x[1],x[2],a[1],b[1],c[1],m[1]);
+		read(y[1],y[2],a[2],b[2],c[2],m[2]);
+		for(i=3;i<=n;i++)
+		{
+			x[i]=(1ll*a[1]*x[i-1]+1ll*b[1]*x[i-2]+c[1])%m[1];
+			y[i]=(1ll*a[2]*y[i-1]+1ll*b[2]*y[i-2]+c[2])%m[2];
+		}
+		d.init();
+		d.add(1);
+		d.add(1000000001);
+		for(i=1;i<=n;i++)
+		{
+			l[i]=min(x[i],y[i])+1;
+			r[i]=max(x[i],y[i])+1;
+			d.add(l[i]);
+			d.add(r[i]);
+			d.add(l[i]+1);
+			d.add(r[i]+1);
+	//		debug(l[i],r[i])
+		}
+		d.work();
+		tr.build(sz(d));
+		for(i=1;i<=n;i++)
+		{
+			tr.upd(d.get_pos(l[i]),d.get_pos(r[i])+1,1);
+			printf("%d\n",tr.ask());
+		}
+	}
+}
+
