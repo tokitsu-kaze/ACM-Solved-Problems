f0a5608f9a209d7aea20ebc502bba2e586e66b2f
tokitsu-kaze <861794979@qq.com>
2019-08-27 18:07:35 +0800

    Create 2019牛客暑期多校训练营（第二场）

---
+++ nowcoder/2019牛客暑期多校训练营（第二场）/882E - MAZE.cpp
@@ -0,0 +1,270 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+	//inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;}
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true;
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=5e4+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+struct Matrix
+{
+	int c[11][11],n;
+	Matrix(){}
+	Matrix(int a,int v=0)
+	{
+		int i,j;
+		n=a;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				c[i][j]=v;
+			}
+		}
+	}
+	Matrix operator *(const Matrix &b)const
+	{
+		int i,j,k;
+		Matrix res(n);
+		for(k=0;k<n;k++)
+		{
+			for(i=0;i<n;i++)
+			{
+				if(!c[i][k]) continue;
+				for(j=0;j<n;j++)
+				{
+					res.c[i][j]+=1ll*c[i][k]*b.c[k][j]%mod;
+					if(res.c[i][j]>=mod) res.c[i][j]-=mod;
+				}
+			}
+		}
+		return res;
+	}
+};
+int m;
+struct Segment_Tree
+{
+	#define type Matrix
+	#define ls (id<<1)
+	#define rs (id<<1|1)
+	int n,ql,qr,pos[MAX];
+	type a[MAX],v[MAX<<2],qv;
+	void pushup(int id)
+	{
+		v[id]=v[ls]*v[rs];
+	}
+	void build(int l,int r,int id)
+	{
+		if(l==r)
+		{
+			pos[l]=id;
+			v[id]=a[l];
+			return;
+		}
+		int mid=(l+r)>>1;
+		build(l,mid,ls);
+		build(mid+1,r,rs);
+		pushup(id);
+	}
+	type res;
+	void query(int l,int r,int id)
+	{
+		if(l>=ql&&r<=qr)
+		{
+			res=res*v[id];
+			return;
+		}
+		int mid=(l+r)>>1;
+		if(ql<=mid) query(l,mid,ls);
+		if(qr>mid) query(mid+1,r,rs);
+	}
+	void build(int _n){n=_n;build(1,n,1);}
+	void upd(int x,type val)
+	{
+		int id=pos[x];
+		v[id]=val;
+		id/=2;
+		while(id>0)
+		{
+			pushup(id);
+			id/=2;
+		}
+	}
+	type ask(int l,int r)
+	{
+		ql=l;
+		qr=r;
+		res=Matrix(m,0);
+		for(int i=0;i<m;i++) res.c[i][i]=1;
+		query(1,n,1);
+		return res;
+	}
+	#undef type
+	#undef ls
+	#undef rs
+}tr;
+Matrix get_mat(int id,char *s)
+{
+	tr.a[id]=Matrix(m,0);
+	for(int i=0;i<m;++i)
+	{
+		if(s[i]=='0')
+		{
+			int p1=i,p2=i;
+			while(p1-1>=0&&s[p1-1]=='0')--p1;
+			while(p2+1<m&&s[p2+1]=='0')++p2;
+			for(int j=p1;j<=p2;++j)
+			{
+				tr.a[id].c[j][i]=1;
+			}
+		}
+	}
+	return tr.a[id];
+}
+char s[MAX][11];
+void go()
+{
+	int n,q,i,op,x,y;
+	Matrix res;
+	while(read(n,m,q))
+	{
+		for(i=1;i<=n;i++)
+		{
+			read(s[i]);
+			get_mat(i,s[i]);
+		}
+		tr.build(n);
+		while(q--)
+		{
+			read(op,x,y);
+			if(op==1)
+			{
+				y--;
+				if(s[x][y]=='1') s[x][y]='0';
+				else s[x][y]='1';
+				tr.upd(x,get_mat(x,s[x]));
+				/*cout<<"upda"<<endl;
+				for(int i=0;i<m;++i)
+				{
+					for(int j=0;j<m;++j)
+					{
+						cout<<get_mat(x,s[x]).c[i][j]<<" ";
+					}
+					cout<<endl;
+				}*/
+			}
+			else
+			{
+				res=tr.ask(1,n);
+				printf("%d\n",res.c[x-1][y-1]);
+			}
+		}
+	}
+}
