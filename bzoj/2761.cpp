bc54705e9c2712eeb5198af416a65f418c5d2874
tokitsu-kaze <861794979@qq.com>
2019-09-26 19:28:24 +0800

    Create 2761.cpp

---
+++ bzoj/2761.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <map>
+#include <list>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <bitset>
+#include <limits>
+#include <assert.h>
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=1e6+10;
+const ll mod=1e9+7;
+/*********************************  head  *********************************/
+void go()
+{
+	int t,n,i,x;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d",&n);
+		VI res;
+		map<int,int> mp;
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&x);
+			if(!mp[x]) res.pb(x);
+			mp[x]=1;
+		}
+		for(i=0;i<sz(res);i++) printf("%d%c",res[i]," \n"[i==sz(res)-1]);
+	}
+}
