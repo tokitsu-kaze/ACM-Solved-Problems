b8550cfbb431b74da9be2c683e20f8916c7212cc
tokitsu-kaze <861794979@qq.com>
2020-03-02 19:00:41 +0800

    Create Codeforces Round #625

---
+++ Codeforces/Codeforces Round #625 (based on Technocup 2020 Final Round)/1320C.cpp
@@ -0,0 +1,245 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+	//inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;} 
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true; 
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze 
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef vector<PII > VPII;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-8;
+const int MAX=1e6+11;
+const ll mod=998244353;
+/*********************************  head  *********************************/
+struct Segment_Tree
+{
+	#define type ll
+	#define ls (id<<1)
+	#define rs (id<<1|1)
+	int n,ql,qr;
+	type a[MAX],v[MAX<<2],tag[MAX<<2],qv;
+	void pushup(int id)
+	{
+		v[id]=max(v[ls],v[rs]);
+	}
+	void pushdown(int id)
+	{
+		if(!tag[id]) return;
+		v[ls]+=tag[id];
+		v[rs]+=tag[id];
+		tag[ls]+=tag[id];
+		tag[rs]+=tag[id];
+		tag[id]=0;
+	}
+	void build(int l,int r,int id)
+	{
+		tag[id]=0;
+		if(l==r)
+		{
+			v[id]=a[l];
+			return;
+		}
+		int mid=(l+r)>>1;
+		build(l,mid,ls);
+		build(mid+1,r,rs);
+		pushup(id);
+	}
+	void update(int l,int r,int id)
+	{
+		if(l>=ql&&r<=qr)
+		{
+			v[id]+=qv;
+			tag[id]+=qv;
+			return;
+		}
+		pushdown(id);
+		int mid=(l+r)>>1;
+		if(ql<=mid) update(l,mid,ls);
+		if(qr>mid) update(mid+1,r,rs);
+		pushup(id);
+	}
+	type res;
+	void query(int l,int r,int id)
+	{
+		if(l>=ql&&r<=qr)
+		{
+			res=max(res,v[id]);
+			return;
+		}
+		pushdown(id);
+		int mid=(l+r)>>1;
+		if(ql<=mid) query(l,mid,ls);
+		if(qr>mid) query(mid+1,r,rs);
+	}
+	void build(int _n){n=_n;build(1,n,1);}
+	void upd(int l,int r,type v)
+	{
+		ql=l;
+		qr=r;
+		qv=v;
+		update(1,n,1);
+	}
+	type ask(int l,int r)
+	{
+		ql=l;
+		qr=r;
+		res=-LLINF;
+		query(1,n,1);
+		return res;
+	}
+	#undef type
+	#undef ls
+	#undef rs
+}tr;
+struct node
+{
+	ll a,b,w;
+	void rd(){read(a,b,w);}
+};
+void go()
+{
+	int n,m,p,i,pos;
+	ll ans,x,y;
+	while(read(n,m,p))
+	{
+		VPII a;
+		for(i=0;i<n;i++)
+		{
+			read(x,y);
+			a.pb(MP(x,y));
+		}
+		for(i=1;i<=MAX-10;i++) tr.a[i]=-LLINF;
+		for(i=0;i<m;i++)
+		{
+			read(x,y);
+			tr.a[x]=max(tr.a[x],-y);
+		}
+		tr.build(MAX-10);
+		vector<node> res;
+		for(i=0;i<p;i++)
+		{
+			node tt;
+			tt.rd();
+			res.pb(tt);
+		}
+		sort(all(res),[&](node a,node b){return a.a<b.a;});
+		sort(all(a));
+		ans=-LLINF;
+		pos=0;
+		for(auto it:a)
+		{
+			while(pos<sz(res)&&res[pos].a<it.fi)
+			{
+				tr.upd(res[pos].b+1,MAX-10,res[pos].w);
+				pos++;
+			}
+			ans=max(ans,tr.ask(1,MAX-10)-it.se);
+	//		debug(it.fi,tr.ask(1,MAX-10))
+		}
+		printf("%lld\n",ans);
+	}
+}
+
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 	Aizu                   7
 	AtCoder               78
 	bzoj                  24
-	Codeforces          1567
+	Codeforces          1570
 	CS Academy            73
 	fzu                   45
 	gym                  148
@@ -19,7 +19,7 @@
 	zoj                  266
 	Other Contest         84
 
-`total : 4252`
+`total : 4255`
 
 
 #### link
