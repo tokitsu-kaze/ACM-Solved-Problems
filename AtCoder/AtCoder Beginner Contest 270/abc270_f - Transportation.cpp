9fd0f614ae34374d0e6882079fd482d58004f32e
tokitsukaze <861794979@qq.com>
2022-11-10 16:37:42 +0800

    2022.11.10

---
+++ AtCoder/AtCoder Beginner Contest 270/abc270_f - Transportation.cpp
@@ -0,0 +1,221 @@
+#include <bits/stdc++.h>
+using namespace std;
+namespace fastIO{
+	#define BUF_SIZE 100000
+	#define OUT_SIZE 100000
+	//fread->read
+	bool IOerror=0;
+	//inline char nc(){char ch=getchar();if(ch==-1)IOerror=1;return ch;} 
+	inline char nc(){
+		static char buf[BUF_SIZE],*p1=buf+BUF_SIZE,*pend=buf+BUF_SIZE;
+		if(p1==pend){
+			p1=buf;pend=buf+fread(buf,1,BUF_SIZE,stdin);
+			if(pend==p1){IOerror=1;return -1;}
+		}
+		return *p1++;
+	}
+	inline bool blank(char ch){return ch==' '||ch=='\n'||ch=='\r'||ch=='\t';}
+	template<class T> inline bool read(T &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(double &x){
+		bool sign=0;char ch=nc();x=0;
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		if(ch=='-')sign=1,ch=nc();
+		for(;ch>='0'&&ch<='9';ch=nc())x=x*10+ch-'0';
+		if(ch=='.'){
+			double tmp=1; ch=nc();
+			for(;ch>='0'&&ch<='9';ch=nc())tmp/=10.0,x+=tmp*(ch-'0');
+		}
+		if(sign)x=-x;
+		return true;
+	}
+	inline bool read(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;!blank(ch)&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read_line(char *s){
+		char ch=nc();
+		for(;blank(ch);ch=nc());
+		if(IOerror)return false;
+		for(;ch!='\n'&&!IOerror;ch=nc())*s++=ch;
+		*s=0;
+		return true;
+	}
+	inline bool read(char &c){
+		for(c=nc();blank(c);c=nc());
+		if(IOerror){c=-1;return false;}
+		return true; 
+	}
+	template<class T,class... U>bool read(T& h,U&... t){return read(h)&&read(t...);}
+	#undef OUT_SIZE
+	#undef BUF_SIZE
+};
+using namespace fastIO;
+/************* debug begin *************/
+string to_string(string s){return '"'+s+'"';}
+string to_string(const char* s){return to_string((string)s);}
+string to_string(const bool& b){return(b?"true":"false");}
+template<class T>string to_string(T x){ostringstream sout;sout<<x;return sout.str();}
+template<class A,class B>string to_string(pair<A,B> p){return "("+to_string(p.first)+", "+to_string(p.second)+")";}
+template<class A>string to_string(const vector<A> v){
+	int f=1;string res="{";for(const auto x:v){if(!f)res+= ", ";f=0;res+=to_string(x);}res+="}";
+	return res;
+}
+void debug_out(){puts("");}
+template<class T,class... U>void debug_out(const T& h,const U&... t){cout<<" "<<to_string(h);debug_out(t...);}
+#ifdef tokitsukaze 
+#define debug(...) cout<<"["<<#__VA_ARGS__<<"]:",debug_out(__VA_ARGS__);
+#else
+#define debug(...) 233;
+#endif
+/*************  debug end  *************/
+#define mem(a,b) memset((a),(b),sizeof(a))
+#define MP make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define sqr(x) (x)*(x)
+using namespace __gnu_cxx;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef pair<ll,ll> PLL;
+typedef pair<int,ll> PIL;
+typedef pair<ll,int> PLI;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef vector<PII > VPII;
+typedef vector<string> VS;
+/************* define end  *************/
+void read(int *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(ll *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void read(double *x,int l,int r){for(int i=l;i<=r;i++) read(x[i]);}
+void println(VI x){for(int i=0;i<sz(x);i++) printf("%d%c",x[i]," \n"[i==sz(x)-1]);}
+void println(VL x){for(int i=0;i<sz(x);i++) printf("%lld%c",x[i]," \n"[i==sz(x)-1]);}
+void println(int *x,int l,int r){for(int i=l;i<=r;i++) printf("%d%c",x[i]," \n"[i==r]);}
+void println(ll *x,int l,int r){for(int i=l;i<=r;i++) printf("%lld%c",x[i]," \n"[i==r]);}
+/*************** IO end  ***************/
+void assert_lr(ll x,ll l,ll r) {assert(x>=l&&x<=r);};
+void go();
+int main(){
+	#ifdef tokitsukaze
+		freopen("TEST.txt","r",stdin);
+	#endif
+	go();return 0;
+}
+const int INF=0x3f3f3f3f;
+const ll LLINF=0x3f3f3f3f3f3f3f3fLL;
+const double PI=acos(-1.0);
+const double eps=1e-6;
+const int MAX=2e5+10;
+const ll mod=998244353;
+/*********************************  head  *********************************/
+struct dsu
+{
+	int pre[MAX];
+	void init(int n)
+	{
+		int i;
+		for(i=1;i<=n;i++) pre[i]=i;
+	}
+	int find(int x)
+	{
+		if(pre[x]!=x) pre[x]=find(pre[x]);
+		return pre[x];
+	}
+	bool merge(int a,int b)
+	{
+		int ra,rb;
+		ra=find(a);
+		rb=find(b);
+		if(ra!=rb)
+		{
+			pre[ra]=rb;
+			return 1;
+		}
+		return 0;
+	}
+}dsu;
+struct Kruskal
+{
+	#define type ll
+	#define inf LLINF
+	vector<pair<type,PII> > e;
+	void init(){e.clear();}
+	void add_edge(int a,int b,type w){e.pb(MP(w,MP(a,b)));}
+	type work(int n)
+	{
+		int x,y,i,cnt;
+		type w;
+		type res=0;
+		dsu.init(n);
+		sort(all(e));
+		for(auto it:e)
+		{
+			x=it.se.fi;
+			y=it.se.se;
+			w=it.fi;
+			if(dsu.merge(x,y)) res+=w;
+		}
+		cnt=0;
+		for(i=1;i<=n;i++) cnt+=dsu.find(i)==i;
+		if(cnt!=1) return inf;
+		return res;
+	}
+	#undef type
+	#undef inf
+}krsk;
+int x[MAX],y[MAX];
+void go()
+{
+	int n,m,i,j,a,b,c;
+	ll ans;
+	while(read(n,m))
+	{
+		read(x,1,n);
+		read(y,1,n);
+		vector<pair<ll,PII> > tmp;
+		while(m--)
+		{
+			read(a,b,c);
+			tmp.pb(MP(c,MP(a,b)));
+		}
+		ans=LLINF;
+		
+		krsk.init();
+		for(auto it:tmp) krsk.add_edge(it.se.fi,it.se.se,it.fi);
+		ans=min(ans,krsk.work(n));
+		
+		krsk.init();
+		for(auto it:tmp) krsk.add_edge(it.se.fi,it.se.se,it.fi);
+		for(i=1;i<=n;i++) krsk.add_edge(i,n+1,x[i]);
+		ans=min(ans,krsk.work(n+1));
+		
+		krsk.init();
+		for(auto it:tmp) krsk.add_edge(it.se.fi,it.se.se,it.fi);
+		for(i=1;i<=n;i++) krsk.add_edge(i,n+1,y[i]);
+		ans=min(ans,krsk.work(n+1));
+		
+		krsk.init();
+		for(auto it:tmp) krsk.add_edge(it.se.fi,it.se.se,it.fi);
+		for(i=1;i<=n;i++) krsk.add_edge(i,n+1,x[i]);
+		for(i=1;i<=n;i++) krsk.add_edge(i,n+2,y[i]);
+		ans=min(ans,krsk.work(n+2));
+		
+		printf("%lld\n",ans);
+	}
+}
